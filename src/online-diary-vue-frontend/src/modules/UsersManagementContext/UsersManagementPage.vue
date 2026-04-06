99f51b6a70a3c5f9e1e7716f023ad919a5cc1355
Captain <demiankira@gmail.com>
2026-04-06 09:09:40 +0700

    модифицирована страница менеджмента пользователей

--- src/online-diary-vue-frontend/src/modules/UsersManagementContext/UsersManagementPage.vue
+++ src/online-diary-vue-frontend/src/modules/UsersManagementContext/UsersManagementPage.vue
@@ -4,6 +4,26 @@ import Card from "@/components/ui/card/Card.vue";
 import Button from "@/components/ui/button/Button.vue";
 import Input from "@/components/ui/input/Input.vue";
 import { PlusIcon, PenIcon } from "lucide-vue-next";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerClose,
+  DrawerDescription,
+} from "@/components/ui/drawer";
+import type { FocusOutsideEvent, PointerDownOutsideEvent } from "reka-ui";
 
 type User = {
   id: number;
@@ -46,9 +66,14 @@ const users = ref<User[]>([
 ]);
 
 const selectedUser = ref<User | null>(users.value[0] ?? null);
+const isSelected = ref<boolean>(false);
 
 function selectUser(u: User) {
   selectedUser.value = u;
+  isSelected.value = true;
+}
+function Handleoutsideclic(event: PointerDownOutsideEvent | FocusOutsideEvent) {
+  isSelected.value = false;
 }
 </script>
 
@@ -59,27 +84,24 @@ function selectUser(u: User) {
     <!-- Комментарий: блок статистики с четырьмя карточками -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
       <Card :class="'p-4 shadow-(--shadow-basic) rounded-sm'">
-        <div class="text-responsive-primary font-semibold">
-          Всего пользователей
+        <div>
+          <div class="text-responsive-primary font-semibold">
+            Всего пользователей
+          </div>
+          <div class="text-responsive-secondary">1 234</div>
+          <div class="text-responsive-primary font-semibold">
+            Пользователей онлайн
+          </div>
+          <div class="text-responsive-secondary">123</div>
         </div>
-        <div class="text-responsive-secondary">1 234</div>
-      </Card>
-
-      <Card :class="'p-4 shadow-(--shadow-basic) rounded-sm'">
-        <div class="text-responsive-primary font-semibold">
-          Пользователей онлайн
+        <div>
+          <div class="text-responsive-primary font-semibold">
+            Преподавателей
+          </div>
+          <div class="text-responsive-secondary">45</div>
+          <div class="text-responsive-primary font-semibold">Студентов</div>
+          <div class="text-responsive-secondary">1 089</div>
         </div>
-        <div class="text-responsive-secondary">123</div>
-      </Card>
-
-      <Card :class="'p-4 shadow-(--shadow-basic) rounded-sm'">
-        <div class="text-responsive-primary font-semibold">Преподавателей</div>
-        <div class="text-responsive-secondary">45</div>
-      </Card>
-
-      <Card :class="'p-4 shadow-(--shadow-basic) rounded-sm'">
-        <div class="text-responsive-primary font-semibold">Студентов</div>
-        <div class="text-responsive-secondary">1 089</div>
       </Card>
     </div>
 
@@ -138,75 +160,98 @@ function selectUser(u: User) {
 
       <!-- Правый блок: информация о выбранном пользователе -->
       <!-- Комментарий: карточка с детальной информацией о пользователе -->
-      <div class="flex flex-col min-h-0 lg:w-1/3 gap-2">
-        <Card :class="'p-4 shadow-(--shadow-basic) rounded-sm'">
-          <h3 class="text-responsive-primary font-semibold">
-            Информация о пользователе
-          </h3>
+      <Sheet>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Редактирование</SheetTitle>
+            <SheetDescription> Информация о пользователе </SheetDescription>
+          </SheetHeader>
+          <div class="flex flex-col min-h-0 lg:w-1/3 gap-2">
+            <div class="mt-3 flex flex-col gap-3">
+              <div>
+                <div class="text-responsive-tertiary text-muted-foreground">
+                  ФИО
+                </div>
+                <div class="text-responsive-secondary">
+                  {{ selectedUser ? selectedUser.name : "-" }}
+                </div>
+              </div>
 
-          <div class="mt-3 flex flex-col gap-3">
-            <div>
-              <div class="text-responsive-tertiary text-muted-foreground">
-                ФИО
+              <div>
+                <div class="text-responsive-tertiary text-muted-foreground">
+                  Логин
+                </div>
+                <div class="text-responsive-secondary">
+                  {{ selectedUser ? selectedUser.login : "-" }}
+                </div>
               </div>
-              <div class="text-responsive-secondary">
-                {{ selectedUser ? selectedUser.name : "-" }}
-              </div>
-            </div>
 
-            <div>
-              <div class="text-responsive-tertiary text-muted-foreground">
-                Логин
+              <div>
+                <div class="text-responsive-tertiary text-muted-foreground">
+                  Почта
+                </div>
+                <div class="text-responsive-secondary">
+                  {{ selectedUser ? selectedUser.email : "-" }}
+                </div>
               </div>
-              <div class="text-responsive-secondary">
-                {{ selectedUser ? selectedUser.login : "-" }}
-              </div>
-            </div>
 
-            <div>
-              <div class="text-responsive-tertiary text-muted-foreground">
-                Почта
+              <div>
+                <div class="text-responsive-tertiary text-muted-foreground">
+                  Подтверждение почты
+                </div>
+                <div class="text-responsive-secondary">
+                  {{
+                    selectedUser
+                      ? selectedUser.emailConfirmed
+                        ? "Да"
+                        : "Нет"
+                      : "-"
+                  }}
+                </div>
               </div>
-              <div class="text-responsive-secondary">
-                {{ selectedUser ? selectedUser.email : "-" }}
-              </div>
-            </div>
 
-            <div>
-              <div class="text-responsive-tertiary text-muted-foreground">
-                Подтверждение почты
+              <div>
+                <div class="text-responsive-tertiary text-muted-foreground">
+                  Последний вход
+                </div>
+                <div class="text-responsive-secondary">
+                  {{ selectedUser ? selectedUser.lastSeen : "-" }}
+                </div>
               </div>
-              <div class="text-responsive-secondary">
-                {{
-                  selectedUser
-                    ? selectedUser.emailConfirmed
-                      ? "Да"
-                      : "Нет"
-                    : "-"
-                }}
-              </div>
-            </div>
 
-            <div>
-              <div class="text-responsive-tertiary text-muted-foreground">
-                Последний вход
-              </div>
-              <div class="text-responsive-secondary">
-                {{ selectedUser ? selectedUser.lastSeen : "-" }}
-              </div>
-            </div>
-
-            <div>
-              <div class="text-responsive-tertiary text-muted-foreground">
-                Роль
-              </div>
-              <div class="text-responsive-secondary">
-                {{ selectedUser ? selectedUser.role : "-" }}
+              <div>
+                <div class="text-responsive-tertiary text-muted-foreground">
+                  Роль
+                </div>
+                <div class="text-responsive-secondary">
+                  {{ selectedUser ? selectedUser.role : "-" }}
+                </div>
               </div>
             </div>
           </div>
-        </Card>
-      </div>
+          <SheetFooter>
+            <Button type="submit"> Save changes </Button>
+            <SheetClose as-child>
+              <Button variant="outline"> Close </Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
     </div>
   </div>
+  <Drawer :open="isSelected" :no-body-styles="true" :direction="'bottom'">
+    <DrawerContent @interact-outside="Handleoutsideclic">
+      <div class="flex flex-col gap-4 p-4">
+        <Input type="FIO" placeholder="ФИО" />
+        <Input type="login" placeholder="Логин" />
+        <Input type="email" placeholder="Почта" />
+        <Input type="Role" placeholder="Роль" />
+      </div>
+      <DrawerClose>
+        <div class="flex p-4 justify-center">
+          <Button v-on:click="isSelected = false"> Закрыть </Button>
+        </div>
+      </DrawerClose>
+    </DrawerContent>
+  </Drawer>
 </template>
