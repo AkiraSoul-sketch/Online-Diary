474c92a0b1baf0e06423162a729f2e47a7aa5dc6
Ramwsh221282 <jimkrauz@gmail.com>
2026-04-20 04:45:48 +0700

    created uuid7 generate implementation

--- src/online-diary-zig-backend/src/Common/Errors/Vvod_Oshibka.zig
+++
@@ -1,4 +0,0 @@
-pub const Vvod_Oshibka = error{
-    /// Ошибка пустого ввода.
-    Pustoy_Vvod,
-};
--- src/online-diary-zig-backend/src/tests.zig
+++
@@ -1,6 +0,0 @@
-const builtin = @import("builtin");
-
-test {
-    _ = @import("tests/Primitives/Stroki_Tests.zig");
-    _ = @import("tests/Primitives/Name_Tests.zig");
-}
